22275feba58eb1698b40539e22d9974fa15380d1
Fernando Kenji <kenjifernandoo@gmail.com>
2020-04-06 22:06:27 -0300

    Challenge done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,68 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repo = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repo)
+
+  return response.json(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if(repoIndex < 0) {
+    return response.status(400).json({ error: "Repository not found!" })
+  }
+
+  const repo = { ...repositories[repoIndex], title, url, techs }
+
+  repositories[repoIndex] = repo
+
+  return response.json(repo)
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+  
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if(repoIndex < 0) {
+    return response.status(400).json({ error: "Repository not found!" })
+  }
+
+  repositories.splice(repoIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  
+  const repo = repositories.find(repo => repo.id === id)
+
+  if(!repo) {
+    return response.status(400).json({ error: "Repository not found!" })
+  }
+  
+  repo.likes++
+
+  return response.json(repo)
 });
 
 module.exports = app;
